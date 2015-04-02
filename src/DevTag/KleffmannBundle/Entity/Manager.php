bd17a4fccfaf227a5a28f87ce78b652485d6db53
Carlos Alberto <karlozz157@gmail.com>
2015-04-02 00:18:56 -0600

    Added manager entity

---
+++ src/DevTag/KleffmannBundle/Entity/Manager.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace DevTag\KleffmannBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="managers")
+ */
+class Manager
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    protected $name;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Manager
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
