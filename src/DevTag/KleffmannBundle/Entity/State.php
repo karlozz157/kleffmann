feefb4e822f0662ca0443196ca5b44a2dd2d6375
Carlos Alberto <karlozz157@gmail.com>
2015-04-02 11:56:17 -0600

    Added external id, country on state entity

--- src/DevTag/KleffmannBundle/Entity/State.php
+++ src/DevTag/KleffmannBundle/Entity/State.php
@@ -5,7 +5,7 @@ namespace DevTag\KleffmannBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="DevTag\KleffmannBundle\Repository\StateRepository")
  * @ORM\Table(name="states")
  */
 class State
@@ -21,6 +21,17 @@ class State
      * @ORM\Column(type="string")
      */
     protected $name;
+
+    /**
+     * @ORM\Column(type="integer", name="external_id")
+     */
+    protected $externalId;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Country")
+     * @ORM\JoinColumn(name="country_id", referencedColumnName="id")
+     */
+    protected $country;
 
     /**
      * Get id
@@ -53,5 +64,51 @@ class State
     public function getName()
     {
         return $this->name;
+    }
+
+    /**
+     * Set externalId
+     *
+     * @param integer $externalId
+     * @return State
+     */
+    public function setExternalId($externalId)
+    {
+        $this->externalId = $externalId;
+
+        return $this;
+    }
+
+    /**
+     * Get externalId
+     *
+     * @return integer
+     */
+    public function getExternalId()
+    {
+        return $this->externalId;
+    }
+
+    /**
+     * Set country
+     *
+     * @param \DevTag\KleffmannBundle\Entity\Country $country
+     * @return State
+     */
+    public function setCountry(\DevTag\KleffmannBundle\Entity\Country $country = null)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return \DevTag\KleffmannBundle\Entity\Country
+     */
+    public function getCountry()
+    {
+        return $this->country;
     }
 }
