d880575f26439c2028f66719c80de80c4669fcdd
Carlos Alberto <karlozz157@gmail.com>
2015-04-06 19:40:04 -0500

    Added constraint on project type

--- src/DevTag/KleffmannBundle/Form/ProjectType.php
+++ src/DevTag/KleffmannBundle/Form/ProjectType.php
@@ -14,39 +14,56 @@ class ProjectType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', 'text', [
+                'required' => true,
+            ])
             ->add('description', 'textarea', [
-                'required' => false
+                'required' => true,
             ])
             ->add('customer', 'entity', [
                 'class' => 'DevTag\KleffmannBundle\Entity\Customer',
                 'property' => 'name',
+                'empty_value' => '-- Choose a Customer --',
+                'required' => true,
             ])
             ->add('methodology', 'entity', [
                 'class' => 'DevTag\KleffmannBundle\Entity\Methodology',
                 'property' => 'name',
+                'empty_value' => '-- Choose a Methodology --',
+                'required' => true,
             ])
-            ->add('fee')
+            ->add('fee', 'money', [
+                'required' => true,
+            ])
             ->add('status', 'entity', [
                 'class' => 'DevTag\KleffmannBundle\Entity\Status',
                 'property' => 'name',
+                'empty_value' => '-- Choose a Status --',
+                'required' => true,
             ])
             ->add('manager', 'entity', [
                 'class' => 'DevTag\KleffmannBundle\Entity\Manager',
                 'property' => 'name',
+                'empty_value' => '-- Choose a Manager --',
+                'required' => true,
             ])
             ->add('projectType', 'entity', [
                 'class' => 'DevTag\KleffmannBundle\Entity\ProjectType',
                 'property' => 'name',
+                'empty_value' => '-- Choose a Project Type --',
+                'required' => true,
             ])
             ->add('startDate', 'date', [
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+                'required' => true,
             ])
             ->add('estimateDate', 'date', [
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+                'required' => true,
             ])
             ->add('endDate', 'date', [
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+                'required' => true,
             ])
         ;
     }
