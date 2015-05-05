ed63889eca55cd84f723ebe08d0e3ba48c047fdf
Carlos Alberto <karlozz157@gmail.com>
2015-05-05 00:04:34 -0500

    Added crud controller on project type uprising controller

--- src/DevTag/KleffmannBundle/Controller/ProjectTypeUprisingController.php
+++ src/DevTag/KleffmannBundle/Controller/ProjectTypeUprisingController.php
@@ -2,103 +2,13 @@
 
 namespace DevTag\KleffmannBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Request;
-use DevTag\KleffmannBundle\Service\Aware\ProjectTypeUprisingAware;
-use DevTag\KleffmannBundle\Controller\Mapped\AbstractController;
-use DevTag\KleffmannBundle\Form\ProjectTypeUprisingType;
-use DevTag\KleffmannBundle\Entity\ProjectTypeUprising;
+use DevTag\KleffmannBundle\Controller\Mapped\CrudController;
 
 /**
  * @Route("/proyectos/tipos-de-levantamientos", service="kleffmann.project_type_uprising.controller")
  */
-class ProjectTypeUprisingController extends AbstractController
+class ProjectTypeUprisingController extends CrudController
 {
-    use ProjectTypeUprisingAware;
 
-    /**
-     * @var array $roles
-     */
-    protected $roles = ['ROLE_PROJECT_MANAGER'];
-
-    /**
-     * @Route("/", name="project_type_uprisings")
-     * @Template()
-     *
-     * @return array
-     */
-    public function indexAction()
-    {
-        $projectTypeUprisings = $this->projectTypeUprisingRepository->findAll();
-
-        return ['projectTypeUprisings' => $projectTypeUprisings];
-    }
-
-    /**
-     * @Route("/nuevo", name="project_type_uprisings_new")
-     * @Template()
-     *
-     * @param Request $request
-     *
-     * @return array|RedirectResponse
-     */
-    public function newAction(Request $request)
-    {
-        $projectTypeUprising = new ProjectTypeUprising();
-        $form = $this->createForm(new ProjectTypeUprisingType(), $projectTypeUprising);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $this->projectTypeUprisingService->save($projectTypeUprising);
-            $this->projectTypeUprisingService->flush();
-
-            return $this->redirectToRoute('project_type_uprisings');
-        }
-
-        return ['form' => $form->createView()];
-    }
-
-    /**
-     * @Route("/editar/{id}", name="project_type_uprisings_edit")
-     * @ParamConverter()
-     * @Template()
-     *
-     * @param ProjectTypeUprising $projectTypeUprising
-     * @param Request $request
-     *
-     * @return array|RedirectResponse
-     */
-    public function editAction(ProjectTypeUprising $projectTypeUprising, Request $request)
-    {
-        $form = $this->createForm(new ProjectTypeUprisingType(), $projectTypeUprising);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $this->projectTypeUprisingService->save($projectTypeUprising);
-            $this->projectTypeUprisingService->flush();
-
-            return $this->redirectToRoute('project_type_uprisings');
-        }
-
-        return ['form' => $form->createView()];
-    }
-
-    /**
-     * @Route("/eliminar/{id}", name="project_type_uprisings_delete")
-     * @ParamConverter()
-     *
-     * @param ProjectTypeUprising $projectTypeUprising
-     *
-     * @return RedirectResponse
-     */
-    public function deleteAction(ProjectTypeUprising $projectTypeUprising)
-    {
-        $this->projectTypeUprisingService->remove($projectTypeUprising);
-        $this->projectTypeUprisingService->flush();
-
-        return $this->redirectToRoute('project_type_uprisings');
-    }
 }
--- src/DevTag/KleffmannBundle/Repository/ProjectTypeUprisingRepository.php
+++ src/DevTag/KleffmannBundle/Repository/ProjectTypeUprisingRepository.php
@@ -6,5 +6,28 @@ use Doctrine\ORM\EntityRepository;
 
 class ProjectTypeUprisingRepository extends EntityRepository
 {
+    use PaginatorAware;
 
+    /**
+     * @param int $page
+     *
+     * @return array
+     */
+    public function findAll($page = null)
+    {
+        $queryBuilder = $this->getEntityManager()
+            ->createQueryBuilder();
+
+        $queryBuilder
+            ->select('ptu')
+            ->from('DevTagKleffmannBundle:ProjectTypeUprising', 'ptu')
+            ->orderBy('ptu.id', 'DESC');
+
+        if (is_null($page)) {
+            return $queryBuilder
+                ->getQuery()->getResult();
+        }
+
+        return $this->paginator->paginate($queryBuilder, $page, $this->recordsPerPage);
+    }
 }
