902bd1f045db808ecb599a34ee39e531433f701e
Carlos Alberto <karlozz157@gmail.com>
2015-05-05 00:42:20 -0500

    Added crud controller on invoice controller

--- src/DevTag/KleffmannBundle/Controller/InvoiceController.php
+++ src/DevTag/KleffmannBundle/Controller/InvoiceController.php
@@ -2,105 +2,14 @@
 
 namespace DevTag\KleffmannBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Symfony\Component\HttpFoundation\Request;
-use DevTag\KleffmannBundle\Controller\Mapped\AbstractController;
-use DevTag\KleffmannBundle\Service\Aware\InvoiceAware;
-use DevTag\KleffmannBundle\Form\InvoiceType;
-use DevTag\KleffmannBundle\Entity\Invoice;
+use DevTag\KleffmannBundle\Controller\Mapped\CrudController;
+
 
 /**
  * @Route("/facturas", service="kleffmann.invoice.controller")
  */
-class InvoiceController extends AbstractController
+class InvoiceController extends CrudController
 {
-    use InvoiceAware;
 
-    /**
-     * @var array $roles
-     */
-    protected $roles = ['ROLE_ADMINISTRATION'];
-
-    /**
-     * @Route("/", name="invoices")
-     * @Template()
-     *
-     * @param Request $request
-     *
-     * @return array
-     */
-    public function indexAction(Request $request)
-    {
-        $page = $request->query->get('page', 1);
-        $invoices = $this->invoiceRepository->findAll($page);
-
-        return ['invoices' => $invoices];
-    }
-
-    /**
-     * @Route("/nuevo", name="invoices_new")
-     * @Template()
-     *
-     * @param Request $request
-     *
-     * @return array
-     */
-    public function newAction(Request $request)
-    {
-        $invoice = new Invoice();
-        $form = $this->createForm(new InvoiceType(), $invoice);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $this->invoiceService->save($invoice);
-            $this->invoiceService->flush();
-
-            return $this->redirectToRoute('invoices');
-        }
-
-        return ['form' => $form->createView()];
-    }
-
-    /**
-     * @Route("/editar/{id}", name="invoices_edit")
-     * @ParamConverter()
-     * @Template()
-     *
-     * @param Invoice $invoice
-     * @param Request $request
-     *
-     * @return array
-     */
-    public function editAction(Invoice $invoice, Request $request)
-    {
-        $form = $this->createForm(new InvoiceType(), $invoice);
-        $form->handleRequest($request);
-
-        if ($form->isValid()) {
-            $this->invoiceService->save($invoice);
-            $this->invoiceService->flush();
-
-            return $this->redirectToRoute('invoices');
-        }
-
-        return ['form' => $form->createView()];
-    }
-
-    /**
-     * @Route("/eliminar/{id}", name="invoices_delete")
-     * @ParamConverter()
-     *
-     * @param Invoice $invoice
-     *
-     * @return array
-     */
-    public function deleteAction(Invoice $invoice)
-    {
-        $this->invoiceService->remove($invoice);
-        $this->invoiceService->flush();
-
-        return $this->redirectToRoute('invoices');
-    }
 }
