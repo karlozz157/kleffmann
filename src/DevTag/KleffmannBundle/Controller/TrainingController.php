9acdb7b44e123b124c548938309609bde7f6b349
Carlos Alberto <karlozz157@gmail.com>
2015-05-07 10:14:13 -0500

    Added notify training action

--- src/DevTag/KleffmannBundle/Controller/TrainingController.php
+++ src/DevTag/KleffmannBundle/Controller/TrainingController.php
@@ -3,12 +3,26 @@
 namespace DevTag\KleffmannBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use DevTag\KleffmannBundle\Controller\Mapped\NotifyActionInterface;
 use DevTag\KleffmannBundle\Controller\Mapped\CrudController;
+use DevTag\KleffmannBundle\Entity\Training;
 
 /**
  * @Route("/capacitaciones", service="kleffmann.training.controller")
  */
-class TrainingController extends CrudController
+class TrainingController extends CrudController implements NotifyActionInterface
 {
-
+    /**
+     * @param string $action
+     * @param Object $entity
+     */
+    public function notifyAction($action, $entity)
+    {
+        if (!$entity instanceof Training) {
+            return;
+        }
+        /** @var Training $training */
+        $training = $entity;
+        $this->service->notifyTraining($action, $training);
+    }
 }
--- src/DevTag/KleffmannBundle/Service/TrainingService.php
+++ src/DevTag/KleffmannBundle/Service/TrainingService.php
@@ -2,7 +2,41 @@
 
 namespace DevTag\KleffmannBundle\Service;
 
+use DevTag\KleffmannBundle\Message\MessageAdapterAware;
+use DevTag\KleffmannBundle\Entity\Interviewer;
+use DevTag\KleffmannBundle\Entity\Training;
+use DevTag\KleffmannBundle\Entity\Message;
+
 class TrainingService extends AbstractService
 {
+    use MessageAdapterAware;
 
+    /**
+     * @param Training $training
+     */
+    public function notifyTraining($action, Training $training)
+    {
+        $action  = str_replace('Action' , '', $action);
+        $action  = ('new' == $action) ? 'generado' : 'editado';
+        $content = 'Se ha %s la capacitacion %s para el proyecto %s. Revisar la plataforma para mas informacion.';
+        $content = sprintf(
+            $content,
+            $action,
+            $training->getName(),
+            $training->getProject()->getName()
+        );
+
+        /** @var Interviewer $interviewer */
+        foreach ($training->getInterviewer() as $interviewer) {
+            $message = new Message();
+            $message->setTo($interviewer->getCellPhone());
+            $message->setContent($content);
+
+            try {
+                $this->messageAdapter->send($message);
+            } catch (\Exception $ex) {
+                // log the exception
+            }
+        }
+    }
 }
