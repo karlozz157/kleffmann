f727f679f6ceee0e50aadd4a282331163f24b2b1
Carlos Alberto <karlozz157@gmail.com>
2015-05-09 22:41:38 -0500

    Added service notify invoice

--- src/DevTag/KleffmannBundle/Service/InvoiceService.php
+++ src/DevTag/KleffmannBundle/Service/InvoiceService.php
@@ -3,9 +3,14 @@
 namespace DevTag\KleffmannBundle\Service;
 
 use DevTag\KleffmannBundle\Repository\InvoiceRepository;
+use DevTag\KleffmannBundle\Sms\SmsAdapterAware;
+use DevTag\KleffmannBundle\Entity\Invoice;
+use DevTag\KleffmannBundle\Entity\Message;
 
 class InvoiceService extends AbstractService
 {
+    use SmsAdapterAware;
+
     /**
      * @var InvoiceRepository $invoiceRepository
      */
@@ -34,6 +39,29 @@ class InvoiceService extends AbstractService
         $newFileName = sprintf('%s_%s', $newFileName, $newVersion);
 
         return sprintf('%s.%s', $newFileName, $fileNameInfo['extension']);
+    }
+
+    /**
+     * @param Invoice $invoice
+     */
+    public function notifyInvoice(Invoice $invoice)
+    {
+        $file = $this->getFileNameInfo($invoice->getFile());
+        $content = sprintf(
+            'La factura %s generado/cambiado con el status %s.',
+            $file['filename'],
+            $invoice->getStatus()
+        );
+
+        $message = new Message();
+        $message->setTo($invoice->getInterviewer()->getCellPhone());
+        $message->setContent(strtoupper($content));
+
+        try {
+            $this->smsAdapter->send($message);
+        } catch (\Exception $ex) {
+            // log the exception
+        }
     }
 
     /**
